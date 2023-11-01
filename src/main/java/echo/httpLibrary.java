f15c456733ba5afedca36da173c6e4e21f05dffa
Shafiq Imtiaz <shafiqimtiaz@gmail.com>
2023-11-01 18:16:16 -0400

    update for A2

--- src/main/java/echo/httpLibrary.java
+++ src/main/java/echo/httpLibrary.java
@@ -32,6 +32,7 @@ public class httpLibrary {
             BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
             BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            System.out.println("Request: \n" + request);
             writeToSocket(wr, request.toString());
             String[] response = readFromSocket(rd);
 
@@ -70,12 +71,12 @@ public class httpLibrary {
 
     public static void post(String data, String path, Socket socket, Map<String, String> headers, boolean verbose, String outputFilePath) throws IOException {
 
+        System.out.println("URL: " + path);
         StringBuilder request = new StringBuilder()
                 .append(String.format("POST %s HTTP/1.0\r\n", path))
                 .append(String.format("Content-Length: %s\r\n", data.length()));
 
         System.out.println();
-
         // Add custom headers to the request
         for (Map.Entry<String, String> entry : headers.entrySet()) {
             request.append(entry.getKey()).append(": ").append(entry.getValue()).append("\r\n");
@@ -86,6 +87,8 @@ public class httpLibrary {
         BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"));
         BufferedReader rd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+        System.out.println("Request: \n" + request.toString());
+        System.out.println("End Request");
         writeToSocket(wr, request.toString());
         String[] response = readFromSocket(rd);
 
@@ -100,7 +103,6 @@ public class httpLibrary {
         if (extractStatusCode(response[0]).compareTo("301") == 0 || extractStatusCode(response[0]).compareTo("302") == 0) {
             System.out.println("--------------- Redirecting -----------------");
             String url = extractLocation(response[0]);
-
             String newURL = "http://" + socket.getInetAddress().getHostName() + url;
             post(data, url, getSocket(newURL), headers, verbose, outputFilePath);
 
@@ -115,16 +117,20 @@ public class httpLibrary {
         rd.close();
     }
 
-    public static void postFile(String filePath, Socket socket, Map<String, String> headers, Boolean verbose, String outputFilePath) {
+    public static void postFile(String url, String filePath, Socket socket, Map<String, String> headers, Boolean verbose, String outputFilePath) {
 
         try (OutputStream os = socket.getOutputStream()) {
             String boundary = "----WebKitFormBoundary" + Long.toHexString(System.currentTimeMillis());
             String requestBody = getRequestBodyForPostFile(filePath, boundary);
-            String requestHeader = getRequestHeaderForPostFile(socket, requestBody.getBytes().length, headers, boundary);
+            String requestHeader = getRequestHeaderForPostFile(url, socket, requestBody.getBytes().length, headers, boundary);
+            System.out.println("Request Header: \n" + requestHeader);
+            System.out.println("Request Body: \n" + requestBody);
 
             // Post the request with file content
-            os.write(requestHeader.getBytes());
-            os.write(requestBody.getBytes());
+            String combinedRequest = requestHeader + requestBody;
+//            os.write(requestHeader.getBytes());
+//            os.write(requestBody.getBytes());
+            os.write(combinedRequest.getBytes());
             os.flush();
 
             // Read and print the response
@@ -177,10 +183,10 @@ public class httpLibrary {
         return requestBodyBuilder.toString();
     }
 
-    private static String getRequestHeaderForPostFile(Socket socket, int size, Map<String, String> headers, String boundary) {
+    private static String getRequestHeaderForPostFile(String path, Socket socket, int size, Map<String, String> headers, String boundary) {
 
         StringBuilder requestHeader = new StringBuilder();
-        requestHeader.append("POST /post HTTP/1.0\r\n")
+        requestHeader.append("POST " + path + " HTTP/1.0\r\n")
                 .append(String.format("Host: %s\r\n", socket.getInetAddress().getHostName()));
 
         // Add custom headers to the request
